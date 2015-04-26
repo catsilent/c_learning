39d287e639f056c487e91d0d5c10e1229ab4016f
catsilent <luojinfu1988@163.com>
2015-04-26 11:21:21 -0400

    add test.c

---
+++ test.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: cat
+	> Mail: luojinfu1988@163.com 
+	> Created Time: Sun 26 Apr 2015 11:10:09 AM EDT
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(void)
+{
+	printf("Hello, World!\n");
+	return 0;
+}
